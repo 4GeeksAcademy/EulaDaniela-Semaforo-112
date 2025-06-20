10f353a6066371017eea8f0ce35af243c4d27acc
Eula Daniela Yamil <euladanielayamil@gmail.com>
2025-06-20 02:12:32 +0000

    semaforo euladaniela112

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,31 @@
-import React from "react";
+
+import React, { useState } from 'react';
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+function Home() {
+	const [colorResaltado, setColorResaltado] = useState(null);
+	const handleClick = (color) => {
+		setColorResaltado(color);
+	}
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	return (
+		<div className="poste-container">
+			<div className="poste m-auto "></div>
+			<div className="container semaforo" style={{ width: '120px' }}>
+				<div className="row justify-content-center">
+					<div onClick={() => handleClick("Red")} className={`Red rounded-circle bg-danger col-sm-4 mt-2 ${colorResaltado === "Red" ? "activo" : ""}`} style={{ width: '75px', height: '75px' }}></div>
+					<div onClick={() => handleClick("Yellow")} className={`Yellow rounded-circle bg-warning col-sm-4 mt-2 ${colorResaltado === "Yellow" ? "activo" : ""}`} style={{ width: '75px', height: '75px' }}></div>
+					<div onClick={() => handleClick("Green")} className={`Green rounded-circle bg-success col-sm-4 mt-2 ${colorResaltado === "Green" ? "activo" : ""}`} style={{ width: '75px', height: '75px' }}></div>
+				</div>
+			</div>
 		</div>
+
+
+
+
 	);
 };
 
